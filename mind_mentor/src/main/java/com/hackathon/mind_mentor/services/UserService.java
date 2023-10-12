cb1fed3bb69053eb1349b3a16dd0febdf6df747e
Kevin <kevinjunchan@gmail.com>
2023-10-12 10:36:50 +0100

    User endpoints added

--- mind_mentor/src/main/java/com/hackathon/mind_mentor/services/UserService.java
+++ mind_mentor/src/main/java/com/hackathon/mind_mentor/services/UserService.java
@@ -1,4 +1,19 @@
 package com.hackathon.mind_mentor.services;
 
+import com.hackathon.mind_mentor.models.User;
+import com.hackathon.mind_mentor.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
 }
