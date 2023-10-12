bfbafcd4841067982bd4ba2979f3a130399c9d6e
rohaib1 <rohaib192@gmail.com>
2023-10-12 09:54:37 +0100

    finished chat model

--- mind_mentor/src/main/java/com/hackathon/mind_mentor/models/Chat.java
+++ mind_mentor/src/main/java/com/hackathon/mind_mentor/models/Chat.java
@@ -1,4 +1,76 @@
 package com.hackathon.mind_mentor.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "chats")
 public class Chat {
+
+    @Id
+    @Column
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private LocalDate date;
+
+    @Column
+    private User user;
+
+    @Column
+    private List<Message> messages;
+
+    public Chat(LocalDate date, User user, List<Message> messages){
+        this.date = date;
+        this.user = user;
+        this.messages = new ArrayList<>();
+    }
+
+    public Chat (){
+
+    }
+
+    public
+    Long getId(){
+        return id;
+    }
+
+    public
+    void setId(Long id){
+        this.id = id;
+    }
+
+    public
+    LocalDate getDate(){
+        return date;
+    }
+
+    public
+    void setDate(LocalDate date){
+        this.date = date;
+    }
+
+    public
+    User getUser(){
+        return user;
+    }
+
+    public
+    void setUser(User user){
+        this.user = user;
+    }
+
+    public
+    List<Message> getMessages(){
+        return messages;
+    }
+
+    public
+    void setMessages(List<Message> messages){
+        this.messages = messages;
+    }
 }
