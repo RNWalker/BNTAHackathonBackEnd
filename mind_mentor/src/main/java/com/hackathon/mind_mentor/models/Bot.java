b998cddddf971430ee79cb680d46660ac1cab2b5
RNWalker <rebecca.noelia.walker@gmail.com>
2023-10-19 15:13:02 +0100

    rebecca has a branch that works

--- mind_mentor/src/main/java/com/hackathon/mind_mentor/models/Bot.java
+++ mind_mentor/src/main/java/com/hackathon/mind_mentor/models/Bot.java
@@ -1,14 +1,18 @@
 package com.hackathon.mind_mentor.models;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Bot {
+    private static ArrayList<ChatMessage> chatHistory = new ArrayList<>();
+
     public static String getGPTResponse(String prompt) {
         String url = "https://api.openai.com/v1/chat/completions";
         String apiKeyPath = "src/main/java/com/hackathon/mind_mentor/key/api_key.txt";
         String apiKey = readApiKeyFromFile(apiKeyPath); // Read API key from the specified path
-        System.out.println(apiKey);
         String model = "gpt-3.5-turbo";
         try {
             URL obj = new URL(url);
@@ -16,8 +20,9 @@ public class Bot {
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Authorization", "Bearer " + apiKey);
             connection.setRequestProperty("Content-Type", "application/json");
-            // The request body
-            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+            chatHistory.add(new ChatMessage("user", prompt));
+            // Include the entire chat history in the request body
+            String body = "{\"model\": \"" + model + "\", \"messages\": " + chatHistoryToJsonArray() + "}";
             connection.setDoOutput(true);
             OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
             writer.write(body);
@@ -26,16 +31,27 @@ public class Bot {
             // Response from ChatGPT
             BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
             while ((line = br.readLine()) != null) {
                 response.append(line);
             }
             br.close();
             // Calls the method to extract the message.
-            return extractMessageFromJSONResponse(response.toString());
+            String botResponse = extractMessageFromJSONResponse(response.toString());
+            chatHistory.add(new ChatMessage("assistant", botResponse)); // Add bot's response to the chat history
+            return botResponse;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String chatHistoryToJsonArray() {
+        StringBuilder jsonArray = new StringBuilder("[");
+        for (ChatMessage message : chatHistory) {
+            jsonArray.append("{\"role\": \"" + message.getRole() + "\", \"content\": \"" + message.getContent() + "\"},");
+        }
+        jsonArray.setCharAt(jsonArray.length() - 1, ']');
+        return jsonArray.toString();
     }
 
     public static String extractMessageFromJSONResponse(String response) {
@@ -74,7 +90,24 @@ public class Bot {
 
             String botResponse = getGPTResponse(userResponse);
             System.out.println(botResponse);
-
         }
+    }
+}
+
+class ChatMessage {
+    private String role;
+    private String content;
+
+    public ChatMessage(String role, String content) {
+        this.role = role;
+        this.content = content;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getContent() {
+        return content;
     }
 }
