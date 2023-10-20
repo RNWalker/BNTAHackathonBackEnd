afef8764fa531520fc5b073097e71725a7a130fd
Kevin <kevinjunchan@gmail.com>
2023-10-20 09:23:23 +0100

    chatbot working for front and backend but needs limit of 255 characters

--- mind_mentor/src/main/java/com/hackathon/mind_mentor/services/MessageService.java
+++ mind_mentor/src/main/java/com/hackathon/mind_mentor/services/MessageService.java
@@ -6,15 +6,74 @@ import com.hackathon.mind_mentor.repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.Scanner;
+
 @Service
 public class MessageService {
 
     @Autowired
     MessageRepository messageRepository;
-    public
-    Message createMessage(Chat chat, LocalDateTime now, boolean b, String text){
+
+    public static String getGPTResponse(String prompt) {
+        String url = "https://api.openai.com/v1/chat/completions";
+        String apiKeyPath = "src/main/java/com/hackathon/mind_mentor/key/api_key.txt";
+        String apiKey = readApiKeyFromFile(apiKeyPath); // Read API key from the specified path
+        System.out.println(apiKey);
+        String model = "gpt-3.5-turbo";
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+            connection.setRequestProperty("Content-Type", "application/json");
+            // The request body
+            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+            connection.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(body);
+            writer.flush();
+            writer.close();
+            // Response from ChatGPT
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+            br.close();
+            // Calls the method to extract the message.
+            return extractMessageFromJSONResponse(response.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static String extractMessageFromJSONResponse(String response) {
+        int start = response.indexOf("content") + 11;
+        int end = response.indexOf("\"", start);
+        return response.substring(start, end);
+    }
+    public static String readApiKeyFromFile(String filePath) {
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            if (scanner.hasNextLine()) {
+                return scanner.nextLine().trim();
+            } else {
+                throw new RuntimeException("API key file is empty.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading API key from file: " + e.getMessage());
+        }
+    }
+    public Message createMessage(Chat chat, LocalDateTime now, boolean b, String text){
         Message message = new Message(chat,now,b,text);
-        return messageRepository.save(message);
+        Message botMessage = new Message(chat,now,true, getGPTResponse(text));
+        messageRepository.save(message);
+        messageRepository.save(botMessage);
+        return message;
     }
 }
